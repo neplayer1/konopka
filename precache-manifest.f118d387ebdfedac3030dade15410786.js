28103460ba5a86fef84bc006c3caaf17a90f619f
nepahka <5294227+neplayer1@users.noreply.github.com>
2019-09-30 18:22:54 +0300

    Updates

---
+++ precache-manifest.f118d387ebdfedac3030dade15410786.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98fe497052c339a816b5b82721dcd887",
+    "url": "/konopka/index.html"
+  },
+  {
+    "revision": "53ec67ed033ede149e54",
+    "url": "/konopka/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "614ebc51a9570faa491e",
+    "url": "/konopka/static/css/main.f8d526d7.chunk.css"
+  },
+  {
+    "revision": "53ec67ed033ede149e54",
+    "url": "/konopka/static/js/2.eac7d014.chunk.js"
+  },
+  {
+    "revision": "614ebc51a9570faa491e",
+    "url": "/konopka/static/js/main.d9c01a08.chunk.js"
+  },
+  {
+    "revision": "f48db025db765a315c12",
+    "url": "/konopka/static/js/runtime~main.6ccc1d52.js"
+  },
+  {
+    "revision": "a16ca331f54e9d31c6a716438290f653",
+    "url": "/konopka/static/media/PT_Root_UI_Regular.a16ca331.woff"
+  },
+  {
+    "revision": "1e0713260bf627251ff5fb0fc3e6daa8",
+    "url": "/konopka/static/media/sample.1e071326.jpg"
+  }
+]);
