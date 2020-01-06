a69e88d5601c445f2fa7f2cb9808bf2eb1a6b3d9
nepahka <5294227+neplayer1@users.noreply.github.com>
2020-01-06 17:32:49 +0300

    test deploy

--- server/app.js
+++ server/app.js
@@ -37,6 +37,6 @@ const dbConnection = mongoose.connection;
 dbConnection.on('error', err => console.log(`Connection error: ${err}!`));
 dbConnection.once('open', () => console.log(`Connected to DB!`));
 
-app.listen(PORT, err => {
+app.listen({ port: process.env.PORT || PORT }, err => {
   err ? console.warn(error) : console.log('Server started!');
 })
