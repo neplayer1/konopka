184827e14ce869ef08fd3e3cbf714eb85ebb2498
nepahka <5294227+neplayer1@users.noreply.github.com>
2020-01-08 14:26:50 +0300

    test server config

--- server/index.js
+++ server/index.js
@@ -8,28 +8,48 @@ const { graphqlUploadExpress } = require('graphql-upload');
 const { existsSync, mkdirSync } = require('fs');
 const { ApolloServer } = require('apollo-server-express');
 
-const connectURI = process.env.MONGODB_URI || 'mongodb://nepahka:t5cmurcXDyP3JtSKZhSZ5J4q@interiordesign-9ryld.mongodb.net/interiorsDB?retryWrites=true&w=majority';
-mongoose.connect(connectURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-      const dbConnection = mongoose.connection;
-      dbConnection.on('error', err => console.log(`Connection error: ${err}!`));
-      dbConnection.once('open', () => console.log(`Connected to DB!`));
-    },
-    err => {
-      console.log('ERROR', err);
-    });
+// const connectURI = process.env.MONGODB_URI || 'mongodb://nepahka:t5cmurcXDyP3JtSKZhSZ5J4q@interiordesign-9ryld.mongodb.net/interiorsDB?retryWrites=true&w=majority';
+// mongoose.connect(connectURI, { useNewUrlParser: true, useUnifiedTopology: true })
+//   .then(() => {
+//       const dbConnection = mongoose.connection;
+//       dbConnection.on('error', err => console.log(`Connection error: ${err}!`));
+//       dbConnection.once('open', () => console.log(`Connected to DB!`));
+//     },
+//     err => {
+//       console.log('ERROR', err);
+//     });
 
-console.log(__dirname);
+const db = process.env.MONGODB_URL || 'mongodb+srv://nepahka:t5cmurcXDyP3JtSKZhSZ5J4q@interiordesign-9ryld.mongodb.net/interiorsDB?retryWrites=true&w=majority';
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true
+    });
+    console.log("MongoDB is Connected...");
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+};
+
+connectDB().then(() => {
+  const dbConnection = mongoose.connection;
+  dbConnection.on('error', err => console.log(`Connection error: ${err}!`));
+  dbConnection.once('open', () => console.log(`Connected to DB!`));
+});
 
 existsSync(path.join(__dirname, "/images")) || mkdirSync(path.join(__dirname, "/images"));
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// app.use(cors({
-//   origin: 'http://localhost:3000',
-//   credentials: true,
-// }));
+app.use(cors({
+  origin: 'https://pure-hollows-87782.herokuapp.com',
+  credentials: true,
+  optionsSuccessStatus: 200
+}));
 
 app.use(cookieParser());
 
