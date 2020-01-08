a49d8e2143113edcef16fec5f2eba1ea893ca141
nepahka <5294227+neplayer1@users.noreply.github.com>
2020-01-08 10:47:16 +0300

    test server config

--- server/index.js
+++ server/index.js
@@ -8,48 +8,51 @@ const { graphqlUploadExpress } = require('graphql-upload');
 const { existsSync, mkdirSync } = require('fs');
 const { ApolloServer } = require('apollo-server-express');
 
-const connectDB = mongoose.connect('mongodb+srv://nepahka:t5cmurcXDyP3JtSKZhSZ5J4q@interiordesign-9ryld.mongodb.net/interiorsDB?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
-connectDB.then(() => {
-  existsSync(path.join(__dirname, "/images")) || mkdirSync(path.join(__dirname, "/images"));
+mongoose.connect('mongodb+srv://nepahka:t5cmurcXDyP3JtSKZhSZ5J4q@interiordesign-9ryld.mongodb.net/interiorsDB?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+      const dbConnection = mongoose.connection;
+      dbConnection.on('error', err => console.log(`Connection error: ${err}!`));
+      dbConnection.once('open', () => console.log(`Connected to DB!`));
+    },
+    err => {
+      console.log('ERROR', err);
+    });
 
-  const app = express();
-  const PORT = process.env.PORT || 5000;
+existsSync(path.join(__dirname, "/images")) || mkdirSync(path.join(__dirname, "/images"));
+
+const app = express();
+const PORT = process.env.PORT || 5000;
 
 // app.use(cors({
 //   origin: 'http://localhost:3000',
 //   credentials: true,
 // }));
 
-  app.use(cookieParser());
+app.use(cookieParser());
 
 // Priority serve any static files.
-  app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
+app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
 
 // All remaining requests return the React app, so it can handle routing.
-  app.get('*', function(request, response) {
-    response.sendFile(path.resolve(__dirname, '../react-ui/build', 'index.html'));
-  });
+app.get('*', function (request, response) {
+  response.sendFile(path.resolve(__dirname, '../react-ui/build', 'index.html'));
+});
 
-  app.use('/images',
-    express.static(path.join(__dirname, "/images")),
-    graphqlUploadExpress({ maxFileSize: 10000000, maxFiles: 20 }),
-  );
+app.use('/images',
+  express.static(path.join(__dirname, "/images")),
+  graphqlUploadExpress({ maxFileSize: 10000000, maxFiles: 20 }),
+);
 
-  const server = new ApolloServer({
-    schema,
-    context: ({ req, res }) => ({ req, res })
-  });
-  server.applyMiddleware({ app, cors: false });
+const server = new ApolloServer({
+  schema,
+  context: ({ req, res }) => ({ req, res })
+});
+server.applyMiddleware({ app, cors: false });
 
-  const dbConnection = mongoose.connection;
-  dbConnection.on('error', err => console.log(`Connection error: ${err}!`));
-  dbConnection.once('open', () => console.log(`Connected to DB!`));
-//
 // app.listen(PORT, err => {
 //   err ? console.warn(error) : console.log(`Server started! listening on port ${PORT}`);
 // })
 
-  app.listen(PORT, function () {
-    console.error(`Node listening on port ${PORT}`);
-  });
+app.listen(PORT, function () {
+  console.error(`Node listening on port ${PORT}`);
 });
