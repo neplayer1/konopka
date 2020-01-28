22ec55825fddf6c941c99291f12d3814e20c1524
nepahka <5294227+neplayer1@users.noreply.github.com>
2020-01-28 16:04:53 +0300

    fix data!

--- react-ui/src/components/CatalogPage/styled/CatalogPageImage.tsx
+++ react-ui/src/components/CatalogPage/styled/CatalogPageImage.tsx
@@ -11,7 +11,7 @@ export const CatalogPageImage: FC<TProps> = ({label, url, imageSrc}) => {
     return (
       <div>
         <Link to={url} data-text={label}>
-            <img src={`/images/${imageSrc}`} alt=""/>
+            <img src={`https://pure-hollows-87782.herokuapp.com/images/${imageSrc}`} alt=""/>
         </Link>
       </div>
     );
